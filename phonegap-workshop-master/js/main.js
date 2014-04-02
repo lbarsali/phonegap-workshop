3b205c643e381fa6bd903f236c35d9c97d96713a
lbarsali <laurent.barsali@ipmfrance.com>
2014-04-02 12:13:46 +0200

    //reset

--- phonegap-workshop-master/js/main.js
+++ phonegap-workshop-master/js/main.js
@@ -1,38 +1,22 @@
 var app = {
 
     findByName: function() {
-        var self = this;
+        console.log('findByName');
         this.store.findByName($('.search-key').val(), function(employees) {
-            $('.employee-list').html(self.employeeLiTpl(employees));
+            var l = employees.length;
+            var e;
+            $('.employee-list').empty();
+            for (var i=0; i<l; i++) {
+                e = employees[i];
+                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
+            }
         });
     },
-    
-    showAlert: function (message, title) {
-        if (navigator.notification) {
-            navigator.notification.alert(message, null, title, 'OK');
-        } else {
-            alert(title ? (title + ": " + message) : message);
-        }
-    },
-    
-    renderHomeView: function() {
-        $('body').html(this.homeTpl());
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
-    
+
     initialize: function() {
-        var self = this;
-        
-        this.homeTpl = Handlebars.compile($("#home-tpl").html());
-        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-        
-        this.store = new MemoryStore(function() {
-            //self.showAlert('Store Initialized', 'Info');
-            self.renderHomeView();
-        });
-        //$('.search-key').on('keyup', $.proxy(this.findByName, this));
+        this.store = new MemoryStore();
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
-    
 
 };
 
