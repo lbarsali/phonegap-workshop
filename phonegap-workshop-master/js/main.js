d77ba3c5b05aff2d273955919fe01d3739d3b0ca
lbarsali <laurent.barsali@ipmfrance.com>
2014-04-02 12:10:37 +0200

    04

--- phonegap-workshop-master/js/main.js
+++ phonegap-workshop-master/js/main.js
@@ -22,13 +22,15 @@ var app = {
     
     initialize: function() {
         var self = this;
+        
+        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+        
         this.store = new MemoryStore(function() {
             //self.showAlert('Store Initialized', 'Info');
             self.renderHomeView();
         });
         //$('.search-key').on('keyup', $.proxy(this.findByName, this));
-        this.homeTpl = Handlebars.compile($("#home-tpl").html());
-        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
     }
     
 
